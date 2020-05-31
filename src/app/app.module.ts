3926178aeb8ebd7d62af5eb352d25dcdfcc1409f
Wellington Max Cruz <wellington.max1@gmail.com>
2020-05-31 20:53:30 -0300

    Organização de imports no core e app-module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 // Angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 // Projeto-Interno
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { LancamentoService } from './lancamentos/lancamento.service';
 import { PessoasModule } from './pessoas/pessoas.module';
-import { PessoasService } from './pessoas/pessoas.service';
-
-// Terceiros
-import { ToastyModule } from 'ng2-toasty';
-import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog'
-import { ConfirmationService } from 'primeng/components/common/api';
 
 @NgModule({
   declarations: [
@@ -29,12 +22,9 @@ import { ConfirmationService } from 'primeng/components/common/api';
     // Projeto-Interno
     CoreModule,
     LancamentosModule,
-    PessoasModule,
-    // Terceiros
-    ToastyModule.forRoot(),
-    ConfirmDialogModule
+    PessoasModule
   ],
-  providers: [LancamentoService, PessoasService, ConfirmationService, {provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,32 @@
 // Angular
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 // Projeto-Interno
 import { NavbarComponent } from './navbar/navbar.component';
+import { ErrorHandlerService } from './error-handler.service';
+import { LancamentoService } from 'app/lancamentos/lancamento.service';
+import { PessoasService } from 'app/pessoas/pessoas.service';
+
+// Terceiros
+import { ConfirmationService } from 'primeng/components/common/api';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { ToastyModule } from 'ng2-toasty';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+     // Terceiros
+     ToastyModule.forRoot(),
+     ConfirmDialogModule
   ],
   declarations: [ NavbarComponent ],
-  exports: [ NavbarComponent ]
+  exports: [ NavbarComponent, ToastyModule, ConfirmDialogModule ],
+  providers: [
+    ErrorHandlerService,
+    LancamentoService,
+    PessoasService,
+    ConfirmationService, {provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
 })
 export class CoreModule { }
