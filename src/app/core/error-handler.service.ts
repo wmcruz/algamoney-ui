9eb02107c4478fae4a95c65491ccd2950d810c34
Wellington Max Cruz <wellington.max1@gmail.com>
2020-05-31 21:51:11 -0300

    Mostrando a mensagem do servico ao usuario.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
 import { ToastyService } from 'ng2-toasty';
 
 @Injectable()
@@ -11,9 +13,21 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
+
+      let errors;
+      msg = 'Erro ao processar a sua solicitação.';
+
+      try {
+        errors = errorResponse.json();
+        msg = errors[0].mensagemUsuario;
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
-      console.log(errorResponse);
+      console.error('Ocorreu um erro', errorResponse);
     }
     this.toasty.error(msg);
   }
