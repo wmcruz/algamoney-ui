896c54f47ccaa12b5ede5a81abb5f7028aedbadd
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-15 21:15:39 -0300

    Alterando classe pessoa e criando Enderço.

--- src/app/core/model.ts
+++ src/app/core/model.ts
@@ -1,5 +1,18 @@
+export class Endereco {
+  logradouro: string;
+  numero: number;
+  complemento: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+}
+
 export class Pessoa {
   codigo: number;
+  nome: string;
+  ativo = true;
+  endereco = new Endereco();
 }
 
 export class Categoria {
