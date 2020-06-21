c0a0c8eec350f327d005ef12887ba48c2ef17711
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-20 21:19:20 -0300

    Implementação de métodos no servico de lançamento: atualizar, buscarPorCodigo e converterStringParaDate.

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -22,7 +22,7 @@ export class LancamentoService {
 
   lancamentosUrl = 'http://localhost:8080/lancamentos';
   // tslint:disable-next-line: max-line-length
-  tokenBearer = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNTkxODQzMzUxLCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiIxMTZhZmM0Ni1kYTAwLTRlOGEtYWRjMC0zYWVkMTIzNTliNWUiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.pyCEuxzcDef2Zq75_esen8cVL7rxD8WwfjcCUp2yOK0';
+  tokenBearer = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNTkyNzAwMzk5LCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiJmZDFlOTY0ZC0wYzhhLTQ2NjctYjQ3ZC1jMjMxZWI1MmJlMDMiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.KQmRSeNSYxdKju491argN1TETJuEDW4lNYr1tmNX_7c';
 
   constructor(private http: Http) { }
 
@@ -78,5 +78,57 @@ export class LancamentoService {
     return this.http.post(this.lancamentosUrl, JSON.stringify(lancamento), { headers })
     .toPromise()
     .then(response => response.json());
+  }
+
+  /**
+   * Método responsável por atualizar um lançamento
+   * @param lancamento
+   */
+  atualizar(lancamento: Lancamento): Promise<Lancamento> {
+    const headers = new Headers();
+    headers.append('Authorization', this.tokenBearer);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.put(`${this.lancamentosUrl}/${lancamento.codigo}`, JSON.stringify(lancamento), { headers })
+    .toPromise()
+    .then(response => {
+      const lancamentoAlterado = response.json() as Lancamento;
+
+      this.converterStringsParaDatas([lancamentoAlterado]);
+
+      return lancamentoAlterado;
+    });
+  }
+
+  /**
+   * Método responsavel por pesquisar um lancamento pelo código
+   * @param codigo
+   */
+  buscarPoCodigo(codigo: number): Promise<Lancamento> {
+    const headers = new Headers();
+    headers.append('Authorization', this.tokenBearer);
+
+    return this.http.get(`${this.lancamentosUrl}/${codigo}`, { headers })
+    .toPromise()
+    .then(response => {
+      const lancamento = response.json() as Lancamento;
+      this.converterStringsParaDatas([lancamento]);
+
+      return lancamento;
+    });
+  }
+
+  /**
+   * Método responsável por converter as datas que vem do BD no formato de String p/ Date
+   * @param lancamentos
+   */
+  private converterStringsParaDatas(lancamentos: Lancamento[]) {
+    for (const lancamento of lancamentos) {
+      lancamento.dataVencimento = moment(lancamento.dataVencimento, 'YYYY-MM-DD').toDate();
+
+      if (lancamento.dataPagamento) {
+        lancamento.dataPagamento = moment(lancamento.dataPagamento, 'YYYY-MM-DD').toDate();
+      }
+    }
   }
 }
