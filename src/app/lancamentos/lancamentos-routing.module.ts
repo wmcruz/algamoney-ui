117d3616dd5d1a2d4e9e39f5a525d1aa4b292ed0
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-21 17:44:04 -0300

    Criação de rotas de lançamentos.

---
+++ src/app/lancamentos/lancamentos-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class LancamentosRoutingModule { }
