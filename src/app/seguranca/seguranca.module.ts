84257ac032db4fd465d67308045ac15f5a9793c7
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-24 23:32:58 -0300

    Implementação de fabrica de autenticação, por aqui todos os serviços do app consomem a config auth.

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -7,6 +7,17 @@ import { ButtonModule } from 'primeng/components/button/button';
 
 import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { Http, RequestOptions } from '@angular/http';
+
+export function authHttpServiceFactory (http: Http, options: RequestOptions) {
+  const config = new AuthConfig({
+    globalHeaders: [
+      { 'Content-Type': 'application/json'}
+    ]
+  });
+  return new AuthHttp(config, http, options);
+}
 
 @NgModule({
   imports: [
@@ -18,6 +29,13 @@ import { LoginFormComponent } from './login-form/login-form.component';
 
     SegurancaRoutingModule
   ],
-  declarations: [LoginFormComponent]
+  declarations: [LoginFormComponent],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
+  ]
 })
 export class SegurancaModule { }
