27626e1d9574ed9b262ca81f65efc24882f06a49
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-24 23:03:57 -0300

    Aprimoramento do Login.

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+import { ErrorHandlerService } from 'app/core/error-handler.service';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -10,13 +13,21 @@ import { AuthService } from '../auth.service';
 export class LoginFormComponent implements OnInit {
 
   constructor(private title: Title,
-    private auth: AuthService) { }
+    private auth: AuthService,
+    private erroHandler: ErrorHandlerService,
+    private router: Router) { }
 
   ngOnInit() {
     this.title.setTitle('Login');
   }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+    .then(() => {
+      this.router.navigate(['/lancamentos']);
+    })
+    .catch(erro => {
+      this.erroHandler.handle(erro);
+    });
   }
 }
