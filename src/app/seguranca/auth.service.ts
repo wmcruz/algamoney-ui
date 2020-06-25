4517f4ef0ec73be846cb9ca1f0f0ef02efda830a
Wellington Max Cruz <wellington.max1@gmail.com>
2020-06-24 23:03:12 -0300

    Armazenando token corretamente e exibindo mensagem correta ao usuário.

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
 
   oauthTokenUtl = 'http://localhost:8080/oauth/token';
+  jwtPayload: any;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http,
+    private jwtHelper: JwtHelper)
+    {
+      this.carregarToken();
+    }
 
   login(usuario: string, senha: string): Promise<void> {
     const headers = new Headers();
@@ -20,10 +26,31 @@ export class AuthService {
     return this.http.post(this.oauthTokenUtl, body, { headers })
     .toPromise()
     .then(response => {
-      console.log(response);
+      this.armazenarToken(response.json().access_token);
     })
     .catch(erro => {
-      console.log(erro);
+      if (erro.status === 400) {
+        const responseJson = erro.json();
+
+        if (responseJson.error === 'invalid_grant') {
+          return Promise.reject('Usuário ou senha inválida!');
+        }
+      }
+
+      return Promise.reject(erro);
     });
+  }
+
+  private armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelper.decodeToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  private carregarToken() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.armazenarToken(token);
+    }
   }
 }
