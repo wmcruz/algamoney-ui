01d1ae36d5c8435062c2f7f36132376583582700
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-10 23:24:06 -0300

    Criação de novos métodos para chamadas correta dos tokens.

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -7,26 +7,23 @@ import { JwtHelper } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
 
-  oauthTokenUtl = 'http://localhost:8080/oauth/token';
+  oauthTokenUrl = 'http://localhost:8080/oauth/token';
   jwtPayload: any;
 
-  constructor(private http: Http,
+  constructor(
+    private http: Http,
     private jwtHelper: JwtHelper) {
       this.carregarToken();
     }
 
-  temPermissao(permissao: string) {
-    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
-  }
-
   login(usuario: string, senha: string): Promise<void> {
     const headers = new Headers();
     headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
-    headers.append('Content-Type', 'application/x-www-form-urlencoded')
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
-    return this.http.post(this.oauthTokenUtl, body, { headers })
+    return this.http.post(this.oauthTokenUrl, body, { headers, withCredentials: true })
     .toPromise()
     .then(response => {
       this.armazenarToken(response.json().access_token);
@@ -44,9 +41,42 @@ export class AuthService {
     });
   }
 
+  obterNovoAccessToken(): Promise<void> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.oauthTokenUrl, body, { headers, withCredentials: true})
+      .toPromise()
+      .then( response => {
+        this.armazenarToken(response.json().access_token);
+
+        console.log('Novo access token criado!');
+
+        return Promise.resolve(null);
+      })
+      .catch( erro => {
+        console.error('Erro ao renovar token.', erro);
+        return Promise.resolve(null);
+
+      });
+  }
+
+  isAccessTokenInvalido() {
+    const token  = localStorage.getItem('token');
+
+    return !token || this.jwtHelper.isTokenExpired(token);
+  }
+
+  temPermissao(permissao: string) {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
+
   private armazenarToken(token: string) {
     this.jwtPayload = this.jwtHelper.decodeToken(token);
-    localStorage.setItem('token', token);
+    localStorage.setItem('token', token); // guarda os dados no navegador do usuário
   }
 
   private carregarToken() {
