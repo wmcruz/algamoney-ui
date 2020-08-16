b1515c8310cbf0b5c61fb87a8d31811b0438f45b
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-16 11:57:11 -0300

    Implementação de serviço que verifica se o usuário logado tem permissão de acesso.

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -74,6 +74,16 @@ export class AuthService {
     return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
   }
 
+  temQualquerPermissao(roles) {
+    for (const role of roles) {
+      if (this.temPermissao(role)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   private armazenarToken(token: string) {
     this.jwtPayload = this.jwtHelper.decodeToken(token);
     localStorage.setItem('token', token); // guarda os dados no navegador do usuário
