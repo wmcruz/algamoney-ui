518cb1eb4ae3b1609644c999c460f37c2c9da7ea
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-23 21:54:43 -0300

    Implementação de canActivate e correção de BUG ao clicar em Nova pessoa.

--- src/app/pessoas/pessoas-routing.module.ts
+++ src/app/pessoas/pessoas-routing.module.ts
@@ -3,11 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { AuthGuard } from 'app/seguranca/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: PessoasPesquisaComponent },
-  { path: ':codigo', component: PessoaCadastroComponent },
-  { path: 'nova', component: PessoaCadastroComponent }
+  {
+    path: '',
+    component: PessoasPesquisaComponent,
+    canActivate: [AuthGuard],
+    data: {roles: ['ROLE_PESQUISAR_PESSOA']}
+  },
+  {
+    path: 'nova',
+    component: PessoaCadastroComponent,
+    canActivate: [AuthGuard],
+    data: {roles: ['ROLE_CADASTRAR_PESSOA']}
+  },
+  {
+    path: ':codigo',
+    component: PessoaCadastroComponent,
+    canActivate: [AuthGuard],
+    data: {roles: ['ROLE_CADASTRAR_PESSOA']}
+  }
 ];
 
 @NgModule({
