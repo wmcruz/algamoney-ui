921a5a0f41cf090dac9257ad6ccf913f4f9c34ac
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-10 15:27:39 -0300

    Novo serviço para dashboard.

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+import { AuthHttp } from 'angular2-jwt';
+import 'rxjs/operator/toPromise';
+import * as moment from 'moment';
+
+import { environment } from 'environments/environment';
+
+@Injectable()
+export class DashboardService {
+
+  lancamentosUrl: string;
+
+  constructor(private http: AuthHttp) {
+    this.lancamentosUrl = `${environment.apiUrl}/lancamentos`;
+  }
+
+  lancamentosPorCategoria(): Promise<Array<any>> {
+
+    return this.http.get(`${this.lancamentosUrl}/estatisticas/por-categoria`)
+    .toPromise()
+    .then(response => response.json());
+  }
+
+  lancamentosPorDia(): Promise<Array<any>> {
+
+    return this.http.get(`${this.lancamentosUrl}/estatisticas/por-dia`)
+    .toPromise(
+    .then(response => {
+      const dados = response.json();
+      this.converterStringParaDatas(dados);
+
+      return dados;
+    });
+  }
+
+  private converterStringParaDatas(dados: Array<any>) {
+    for (const dado of dados) {
+      dado.dia = moment(dado.dia, 'YYYY-MM-DD').toDate();
+    }
+  }
+}
