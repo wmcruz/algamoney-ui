b058dac709b542b37bce9bfae4ae81fe710e3fae
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-10 18:58:16 -0300

    Alteração do metodo para gerar pdf, chamando o nosso service.

--- src/app/relatorios/relatorios-lancamentos/relatorios-lancamentos.component.ts
+++ src/app/relatorios/relatorios-lancamentos/relatorios-lancamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RelatoriosService } from '../relatorios.service';
 
 @Component({
   selector: 'app-relatorios-lancamentos',
@@ -10,13 +11,17 @@ export class RelatoriosLancamentosComponent implements OnInit {
   periodoInicio: Date;
   periodoFim: Date;
 
-  constructor() { }
+  constructor(private relatoriosService: RelatoriosService) { }
 
   ngOnInit() {
   }
 
   gerar() {
-    console.log(this.periodoInicio);
-    console.log(this.periodoFim);
+    this.relatoriosService.relatorioLancamentosPorPessoa(this.periodoInicio, this.periodoFim)
+    .then(relatorio => {
+      const url = window.URL.createObjectURL(relatorio);
+
+      window.open(url);
+    });
   }
 }
