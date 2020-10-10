e6d96f85f625b256be1d08868282a9e806aa1fc1
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-10 15:12:16 -0300

    Adicionando modulo do chart.js

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-
 @NgModule({
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
+
     SharedModule,
     DashboardRoutingModule
   ],
